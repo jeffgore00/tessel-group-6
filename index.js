47c9e269c5eb6ab2b93bb8f9338869e5f97d446c
Ben Ellentuck <ellentuckben@gmail.com>
2018-03-19 17:39:21 -0400

    working mvp

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var tessel = require('tessel');
 var accel = require('accel-mma84').use(tessel.port['A']);
 var happy = './audio/HappyIntro.mp3';
 // var dark = './audio/DarkIntro.mp3';
+var tagline = './audio/danceYourAxisOff.mp3';
 var fileToPlay;
 
 var path = require('path');
@@ -17,13 +18,31 @@ var sound = new av.Player(mp3);
 // });
 
 
+
 // var sound = require('../audio/audio');
 // Initialize the accelerometer.
 accel.on('ready', function () {
+
   console.log("ready!!!!!!!!!!!!!!!!!")
 // Stream accelerometer data
+// let openinghasplayed = false;
+// let time = 0;
+
+// if (!openinghasplayed) {
+//   console.log('in the condition')
+//   const newMp3 = path.join(__dirname, tagline);
+//   const opening = new av.Player(newMp3);
+//   opening.play();
+//   console.log('play line passed')
+//   openinghasplayed = true;
+// }
+// setTimeout(() => {
  setInterval(function(){
 accel.on('data', function (xyz) {
+
+
+
+
   // setInterval(function(){
     // while (true){
     console.log('x:', xyz[0].toFixed(2),
@@ -35,17 +54,17 @@ accel.on('data', function (xyz) {
         sound.pause();
       }
     })
- }, 100)
+ }, 100);
+
 
   // var mp3 = path.join(__dirname, fileToPlay);
   // var sound = new av.Player(mp3);
   // sound.play()
-  // }, 100)
+  // }, 100;
 
+// }, 6000);
 
 });
-
-// });
 
 accel.on('error', function(err){
   console.log('Error:', err);
